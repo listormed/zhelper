a1dabfb8b9bc56e1c895cea895e4103b47fb812d
listormed <listormed@proton.me>
2023-01-20 01:16:40 +0500

    Initial config

---
+++ src/test/java/ru/krsmon/zhelper/MockData.java
@@ -0,0 +1,4 @@
+package ru.krsmon.zhelper;
+
+abstract class MockData {
+}
--- src/test/java/ru/krsmon/zhelper/ZabbixHelperAbstractTest.java
+++ src/test/java/ru/krsmon/zhelper/ZabbixHelperAbstractTest.java
@@ -1,4 +1,15 @@
 package ru.krsmon.zhelper;
 
-public abstract class ZabbixHelperAbstractTest {
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ActiveProfiles(profiles = "test")
+@SpringBootTest(classes = ZabbixHelperApplication.class)
+@TestPropertySource(locations = "/application-test.yaml")
+@ExtendWith({ SpringExtension.class, OutputCaptureExtension.class })
+public abstract class ZabbixHelperAbstractTest extends MockData {
 }
